30c7448d2c26fd8655d13ad1d20df3c69c6f487e
Victor Hugo Borja <vborja@apache.org>
2026-05-08 15:01:31 -0600

    hello-world example

---
+++ templates/ci/modules/tests/readme/hello-world.nix
@@ -0,0 +1,33 @@
+# Simple hello-world example showcases a single computation running
+# on different evironments (effect context).
+{
+  lib,
+  ned,
+  ci,
+  ...
+}:
+let
+  inherit (ned) run st ctx-d;
+
+  # language contextual drivers
+  en-d = ctx-d { hello = "hello"; world = "world"; };
+  sp-d = ctx-d { hello = "hola"; world = "mundo"; };
+
+  # stream of contextual computations
+  comp-s = middle: st ({ hello }: hello) middle ({ world }: world) ({ hello }: hello);
+
+  english = comp-s st en-d;
+  spanglish = comp-s en-d sp-d;
+
+  expr = {
+    english = lib.concatStringsSep " " english.toList;
+    spanglish = lib.concatStringsSep " " spanglish.toList;
+  };
+  expected = {
+    english = "hello world hello";
+    spanglish = "hello mundo hola";
+  };
+in
+{
+  flake.tests.readme.test-hello-world = { inherit expr expected; };
+}
